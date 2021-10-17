fd8eb4494a5080727b390899f2def40a0f57f224
xzwnp <2633247964@qq.com>
2021-10-17 23:15:14 +0800

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * PACKAGE_NAME
+ *
+ * @author xzwnp
+ * 2021/10/17
+ * 23:14
+ * @Description ：
+ * Steps：
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
